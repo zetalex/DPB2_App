7cfdf8aef2e885a1f24248401be6f1237ceea3d8
Borja Martínez Sánchez <bmarsan@upv.es>
2024-04-16 12:14:40 +0200

    Cleaned up CMD Python code

--- ZMQ_Test_App/ZMQ_Test_App_cmd.py
+++ ZMQ_Test_App/ZMQ_Test_App_cmd.py
@@ -1,31 +1,5 @@
 import zmq
-import numpy as np
-import matplotlib.pyplot as plt
 
-temp_val = np.tile(np.float32(0),10)
-timestamp = np.tile(0,10)
-init_timestamp = np.tile(0,1)
-
-
-# def process_json(json_data,i):
-#     # Parsear el JSON
-#     json_obj = json.loads(json_data)
-#     if i == 0 :
-#         init_timestamp[0] = json_obj['timestamp']
-#         timestamp[0] = 0
-#     else:
-#         timestamp[i] = json_obj['timestamp'] - init_timestamp[0]
-#     # Extraer información de cada campo
-#     data = json_obj['data']
-
-#     # # Extraer información de los subcampos de 'data'
-#     dpb_data = data['DPB']
-#     pl_temp = dpb_data[10]
-#     temp_val[i] = pl_temp['value']
-#     print(pl_temp['value'])
-    
-#     # # Imprimir la información extraída
-#     #print(json.dumps(json_obj, indent=4))
 
 def main():
     context = zmq.Context()
