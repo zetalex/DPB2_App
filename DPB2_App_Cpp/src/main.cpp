37b3a32c433c4a43c9083f5b0edc915b8851658a
Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>
2024-10-07 11:25:34 +0200

    First version of digitizer monitoring
    Signed-off-by: Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>

--- DPB2_App_Cpp/src/main.cpp
+++ DPB2_App_Cpp/src/main.cpp
@@ -432,6 +432,100 @@ static void *monitoring_thread(void *arg)
 			parsing_mon_environment_data_into_object(jdpb,curr, curr_som[l]);
 			parsing_mon_environment_data_into_object(jdpb,pwr, curr_som[l]*volt_som[l]);
 		}
+
+		//Digitizer 0 Slow Control Monitoring
+		CCOPacket pkt(COPKT_DEFAULT_START, COPKT_DEFAULT_STOP, COPKT_DEFAULT_SEP);
+		COPacketResponse_type	pktError=COPACKET_NOERR;
+		char digcmd[32];
+		char dig_response[32];
+		char *dig_mag_str;
+		dig_mag_str = (char*) malloc(16);
+		// Board parameters
+		for(int i = 0; i < sizeof(dig_monitor_mag_board_codes); i++){
+			pkt.CreatePacket(digcmd, HkDigCmdList.CmdList[dig_monitor_mag_board_codes[i]].CmdString);
+			dig_command_handling(0,digcmd,dig_response);
+			pktError = pkt.LoadString(dig_response);
+			int16_t cmdIdx = pkt.GetNextFiedlAsCOMMAND(HkDigCmdList);
+			if(cmdIdx == HKDIG_ERRO){
+				strcpy(dig_mag_str,"ERROR");
+			}
+			else{
+				dig_mag_str = pkt.GetNextField();
+				if(cmdIdx == HKDIG_GET_CLOCK)
+					if(!strcmp(dig_mag_str,"0")){
+						strcpy(dig_mag_str,"Local");
+					}
+					else{
+						strcpy(dig_mag_str,"DPB");
+					}
+				parsing_mon_environment_string_into_object(jdig0, dig_monitor_mag_board_names[i],dig_mag_str);
+			}
+		}
+
+		// Channel parameters
+		json_object *jdig0channels = json_object_new_array();
+		for(int i = 0; i < sizeof(dig_monitor_mag_chan_codes); i++){
+				for(int j = 0; j < 12; j++){
+				pkt.CreatePacket(digcmd, HkDigCmdList.CmdList[dig_monitor_mag_chan_codes[i]].CmdString,(uint32_t) j);
+				dig_command_handling(0,digcmd,dig_response);
+				pktError = pkt.LoadString(dig_response);
+				int16_t cmdIdx = pkt.GetNextFiedlAsCOMMAND(HkDigCmdList);
+				if(cmdIdx == HKDIG_ERRO){
+					strcpy(dig_mag_str,"ERROR");
+				}
+				else{
+					dig_mag_str = pkt.GetNextField();
+					float dig_value = atof(dig_mag_str);
+					parsing_mon_channel_data_into_object(jdig0channels,j, dig_monitor_mag_chan_names[i],dig_value);
+				}
+			}
+		}
+		json_object_object_add(jdig0,"channels",jdig0channels);
+
+		//Digitizer 1 Slow Control Monitoring
+
+		// Board parameters
+		for(int i = 0; i < sizeof(dig_monitor_mag_board_codes); i++){
+			pkt.CreatePacket(digcmd, HkDigCmdList.CmdList[dig_monitor_mag_board_codes[i]].CmdString);
+			dig_command_handling(1,digcmd,dig_response);
+			pktError = pkt.LoadString(dig_response);
+			int16_t cmdIdx = pkt.GetNextFiedlAsCOMMAND(HkDigCmdList);
+			if(cmdIdx == HKDIG_ERRO){
+				strcpy(dig_mag_str,"ERROR");
+			}
+			else{
+				dig_mag_str = pkt.GetNextField();
+				if(cmdIdx == HKDIG_GET_CLOCK)
+					if(!strcmp(dig_mag_str,"0")){
+						strcpy(dig_mag_str,"Local");
+					}
+					else{
+						strcpy(dig_mag_str,"DPB");
+					}
+				parsing_mon_environment_string_into_object(jdig0, dig_monitor_mag_board_names[i],dig_mag_str);
+			}
+		}
+
+		// Channel parameters
+		json_object *jdig1channels = json_object_new_array();
+		for(int i = 0; i < sizeof(dig_monitor_mag_chan_codes); i++){
+				for(int j = 0; j < 12; j++){
+				pkt.CreatePacket(digcmd, HkDigCmdList.CmdList[dig_monitor_mag_chan_codes[i]].CmdString,(uint32_t) j);
+				dig_command_handling(1,digcmd,dig_response);
+				pktError = pkt.LoadString(dig_response);
+				int16_t cmdIdx = pkt.GetNextFiedlAsCOMMAND(HkDigCmdList);
+				if(cmdIdx == HKDIG_ERRO){
+					strcpy(dig_mag_str,"ERROR");
+				}
+				else{
+					dig_mag_str = pkt.GetNextField();
+					float dig_value = atof(dig_mag_str);
+					parsing_mon_channel_data_into_object(jdig1channels,j, dig_monitor_mag_chan_names[i],dig_value);
+				}
+			}
+		}
+		json_object_object_add(jdig1,"channels",jdig1channels);
+
 		//LV Slow Control Monitoring
 		char lv_mon_root[80];
 		char lv_mon_cmd[80];
