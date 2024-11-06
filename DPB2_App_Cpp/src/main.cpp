ac0f18ab13ea3f5e8d3f65f113774e83cc5b980d
Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>
2024-11-06 15:45:48 +0100

    Fix RXLOS and TXFAULT on monitoring
    Signed-off-by: Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>

--- DPB2_App_Cpp/src/main.cpp
+++ DPB2_App_Cpp/src/main.cpp
@@ -402,8 +402,8 @@ static void *monitoring_thread(void *arg)
 			parsing_mon_channel_data_into_object(jsfps,i,"txpwr",sfp_txpwr[i]);
 			parsing_mon_channel_data_into_object(jsfps,i,"rxpwr",sfp_rxpwr[i]);
 
-			parsing_mon_channel_status_into_object(jsfps,0,"rxlos",sfp_status[0][i]);
-			parsing_mon_channel_status_into_object(jsfps,0,"txfault",sfp_status[1][i]);
+			parsing_mon_channel_status_into_object(jsfps,i,"rxlos",sfp_status[0][i]);
+			parsing_mon_channel_status_into_object(jsfps,i,"txfault",sfp_status[1][i]);
 			}
 		}
 		parsing_mon_environment_data_into_object(jdpb,"lpdcputemp", ams_temp[0]);
