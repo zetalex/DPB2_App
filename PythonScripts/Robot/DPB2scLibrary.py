b9e147c14ec42703906515274cef94d11c45ed6f
Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>
2025-09-25 16:13:59 +0200

    Remove turning off DMA Enable
    The bug must be solved
    
    Signed-off-by: Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>

--- PythonScripts/Robot/DPB2scLibrary.py
+++ PythonScripts/Robot/DPB2scLibrary.py
@@ -154,8 +154,7 @@ class DPB2scLibrary(object):
         # Turn on only RS485 Main Driver
         self.write_gpio(68,"ON")
         self.write_gpio(69,"OFF")
-        # Turn off DMA
-        self.write_gpio(57,"OFF")
+
         # Send termination signal to the libdpb2sc library
         self.dpb2sc.dpbsc_lib_close(self.structure_i2c)
         
