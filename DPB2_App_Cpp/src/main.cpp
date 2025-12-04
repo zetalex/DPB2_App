2326fd0d4a4c1be576d364e75b8b396d9d61acb7
Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>
2025-12-04 14:15:40 +0100

    Move monitoring odsel to board parameters
    Signed-off-by: Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>

--- DPB2_App_Cpp/src/main.cpp
+++ DPB2_App_Cpp/src/main.cpp
@@ -619,6 +619,7 @@ static void *monitoring_thread(void *arg)
 					case HKDIG_GET_PED_STAGGER:
 					case HKDIG_GET_PED_PERIOD:
 					case HKDIG_GET_TB_REG:
+					case HKDIG_GET_OD_SEL_REG:
 						dig_mag_str=pkt.GetNextField();
 						parsing_mon_environment_string_into_object(jdig0, dig_monitor_mag_board_names[i],dig_mag_str);
 						break;
@@ -685,14 +686,6 @@ static void *monitoring_thread(void *arg)
 			json_object *jdig0channels = json_object_new_array();
 			for(int i = 0; i < DIG_MON_CHAN_CODES_SIZE; i++){
 					for(int j = 0; j < 18; j++){
-					if(dig_monitor_mag_chan_codes[i] == HKDIG_GET_OD_SEL_REG){   //Only for OD channels
-						if(j < 12){
-							continue;
-						}
-						else{
-							j = j - 12;
-						} 
-					}
 					pkt.CreatePacket(digcmd, HkDigCmdList.CmdList[dig_monitor_mag_chan_codes[i]].CmdString,(uint32_t) j);
 					rc = dig_command_handling(DIGITIZER_0,digcmd,dig_response);
 					if(rc){ // If the function returns error, we cancel reading on this board until check_board_presence() returns that it is available again
@@ -710,8 +703,6 @@ static void *monitoring_thread(void *arg)
 							break;
 
 						//String
-						case HKDIG_GET_OD_SEL_REG:   //Only for OD channels, restore real channel number
-						j = j + 12;
 						case HKDIG_GET_THR_NUM:
 						case HKDIG_GET_CHN_STATUS:
 						case HKDIG_GET_CHN_CNTRL:
@@ -768,6 +759,7 @@ skip_dig0:
 					case HKDIG_GET_PED_STAGGER:
 					case HKDIG_GET_PED_PERIOD:
 					case HKDIG_GET_TB_REG:
+					case HKDIG_GET_OD_SEL_REG:
 						dig_mag_str=pkt.GetNextField();
 						parsing_mon_environment_string_into_object(jdig1, dig_monitor_mag_board_names[i],dig_mag_str);
 						break;
@@ -834,14 +826,6 @@ skip_dig0:
 			json_object *jdig1channels = json_object_new_array();
 			for(int i = 0; i < DIG_MON_CHAN_CODES_SIZE; i++){
 					for(int j = 0; j < 18; j++){
-					if(dig_monitor_mag_chan_codes[i] == HKDIG_GET_OD_SEL_REG){   //Only for OD channels
-						if(j < 12){
-							continue;
-						}
-						else{
-							j = j - 12;
-						} 
-					}
 					pkt.CreatePacket(digcmd, HkDigCmdList.CmdList[dig_monitor_mag_chan_codes[i]].CmdString,(uint32_t) j);
 					rc = dig_command_handling(DIGITIZER_1,digcmd,dig_response);
 					if(rc){ // If the function returns error, we cancel reading on this board until check_board_presence() returns that it is available again
@@ -860,8 +844,6 @@ skip_dig0:
 							break;
 
 						//String
-						case HKDIG_GET_OD_SEL_REG:   //Only for OD channels, restore real channel number
-						j = j + 12;
 						case HKDIG_GET_THR_NUM:
 						case HKDIG_GET_CHN_STATUS:
 						case HKDIG_GET_CHN_CNTRL:
